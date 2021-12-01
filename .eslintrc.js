017a350e9b942f25be3616fdd43bf53ba2725b97
Matt Brophy <matt@brophy.org>
2021-12-01 10:13:12 -0500

    Lift eslint to consumer apps (#108)
    * Remove eslint deps
    
    * Add back eslint deps as dev deps
    
    * Update config
    
    * Bump version 7.0.0-beta.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,10 +12,6 @@ module.exports = {
     },
     extends: [
         'airbnb-base',
-        'plugin:vue/recommended',
-    ],
-    plugins: [
-        'vue',
     ],
     rules: {
         // Kept from airbnb-base@13, that switched to 'always' in 14
@@ -46,16 +42,5 @@ module.exports = {
         'padded-blocks': 'off',
         // Don't enforce promises being rejected with Error objects
         'prefer-promise-reject-errors': 'off',
-        // Use 4 space indents in templates
-        'vue/html-indent': ['error', 4],
-        // Allow max 2 attributes on a single line element, but once the
-        // element is spread across multiple, require one attribute per line
-        'vue/max-attributes-per-line': ['error', {
-            singleline: 3,
-            multiline: {
-                max: 1,
-                allowFirstLine: true,
-            },
-        }],
     },
 };
